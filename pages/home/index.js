a41e070b45d8bf5c755de898c2926ee359fe5a27
Nicolas <109254154+kZNick@users.noreply.github.com>
2022-10-13 17:10:00 -0300

    Projeto Finalizado

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,71 @@
-/* Desenvolva sua lógica aqui...*/
+const inputMain = document.getElementById("inputUser")
+const buttonMain = document.querySelector(".input-button")
+const surgestaoes = document.querySelector(".surgestaoes")
+
+function LocationUserRecente(){
+    return JSON.parse(localStorage.getItem("usersRecentes")) || []
+}
+//funçao principal
+async function Buscarusuario() {
+    const ususarioIvalido = document.querySelector('.input-erro');
+    try {
+        buttonMain.innerHTML = `<img src="assets/lodin.gif" alt="" width="90px">`
+        const data = await fetch(`https://api.github.com/users/${inputMain.value}`);
+        const dataJSON = await data.json();
+        if(dataJSON.message === "Not Found"){
+         throw "Usuario ivalido"
+        }
+    let user = LocationUserRecente()
+    user = [...user, dataJSON]
+    if(user.length >= 4){
+        user.splice(0,1)
+        localStorage.setItem("usersRecentes", JSON.stringify(user))
+    }
+    localStorage.setItem("usersRecentes", JSON.stringify(user))
+    buttonMain.innerHTML = `Ver perfil do github`
+    localStorage.setItem("userLogado",inputMain.value)
+    window.location.replace("../../pages/profile/index.html")
+    } catch {
+    ususarioIvalido.classList.add("display-on")
+    buttonMain.innerHTML = `Ver perfil do github`
+    console.log('Usuario Ivalido');
+    }
+  }
+  
+
+//form nao atulizar a pagina 
+const formTag = document.querySelector('form')
+formTag.addEventListener('submit',(event)=>{
+    event.preventDefault()})
+
+inputMain.addEventListener("keyup",(event)=>{
+    buttonMain.classList.add("input-button-on")
+    if(inputMain.value === ""){
+        buttonMain.classList.remove("input-button-on")
+    }
+})
+//botao
+buttonMain.addEventListener("click",async()=>{
+    if(inputMain.value === ""){
+        return "sem nada no input"
+    }
+    Buscarusuario()
+})
+
+function recentesUsers(){   
+    const localUsers = LocationUserRecente()
+    
+    localUsers.forEach(element => {
+    const button =  document.createElement("button")
+    button.classList.add("surgestaoes-button")
+    surgestaoes.appendChild(button)
+    button.innerHTML = `<img src=${element.avatar_url} alt="" class="user-img">
+    <p>Acessar este perfil</p>`
+
+    button.addEventListener("click",()=>{
+        localStorage.setItem("userLogado",element.login)
+        window.location.replace("../../pages/profile/index.html")
+    })
+    })
+}
+recentesUsers()
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,103 @@
-/* Desenvolva sua lógica aqui...*/
+const imgPerfil = document.querySelector(".perfil-img")
+const namePerfil = document.querySelector(".perfil-name")
+const bioPerfil = document.querySelector(".perfil-bio")
+const trocarPerfil = document.querySelector(".button-trocarPerfil")
+const repositorioss = document.querySelector(".repositorios")
+const loading = document.querySelector(".loading")
+const titlepage = document.querySelector("title")
+const modalErro = document.querySelector(".modalErro")
+const buttonEmail = document.querySelector(".button-email")
+const modalTitulo = document.querySelector(".modal-titulo")
+
+let userLogado = localStorage.getItem("userLogado")
+if(userLogado === null){
+    alert("Usuario inválido")
+    localStorage.removeItem("userLogado")
+    window.location.replace("../../index.html")
+}
+//header
+async function perfilUsuario(user){
+    try{   
+        const data = await fetch(`https://api.github.com/users/${user}`)
+        const dataJSON = await data.json()
+        imgPerfil.src = dataJSON.avatar_url
+        titlepage.innerText = dataJSON.login
+        namePerfil.innerText = dataJSON.name
+        bioPerfil.innerText = dataJSON.bio
+        buttonEmail.innerText = "Email"
+
+        imgPerfil.addEventListener("click",()=>{
+            window.open(dataJSON.html_url, "_blank");
+        })
+
+        buttonEmail.addEventListener("click",()=>{
+            if(dataJSON.email === null){
+                modalTitulo.innerText = "E-mail do Usuário está privado"
+                modalErro.classList.remove("modal-on")
+                modalErro.classList.add("modal-on")
+                return setTimeout(()=>{
+                    modalErro.classList.remove("modal-on")
+                }, 3000)
+            }
+            window.open(`mailto:${dataJSON.email}`);
+        })
+
+        console.log(dataJSON)
+    }catch {
+        console.log('Usuario Ivalido');
+        localStorage.removeItem("userLogado")
+        window.location.replace("../../index.html")
+        }
+}
+perfilUsuario(userLogado)
+//main
+async function userRepositorios(user){
+    try{   
+        loading.classList.add("flex-on")
+        const repositorios = await fetch(`https://api.github.com/users/${user}/repos`)
+        const repositoriosJSON = await repositorios.json()
+        repositoriosJSON.forEach(element => {
+            const repositorio = document.createElement("li")
+            repositorio.classList.add("repositorio")
+            const repositorioTitulo = document.createElement("h2")
+            repositorioTitulo.classList.add("repositorio-titulo")
+            const repositorioDecricao = document.createElement("p")
+            repositorioDecricao.classList.add("repositorio-decricao")
+            const repositorioBotoes = document.createElement("div")
+            repositorioBotoes.classList.add("repositorio-botoes")
+            const butaoRepsitorio = document.createElement("button")
+            butaoRepsitorio.classList.add("butao-repsitorio")
+            const butaoDemo = document.createElement("button")
+            butaoDemo.classList.add("butao-demo")
+
+            repositorioss.appendChild(repositorio)
+            repositorio.append(repositorioTitulo,repositorioDecricao,repositorioBotoes)
+            repositorioBotoes.append(butaoRepsitorio,butaoDemo)
+            repositorioTitulo.innerText = element.name
+            repositorioDecricao.innerText = element.description
+            butaoRepsitorio.innerHTML = `<a href="${element.clone_url}" target="_blank">Repositório</a>`
+            butaoDemo.innerText = "Demo"
+            butaoDemo.addEventListener("click",()=>{
+                if(element.homepage === null){
+                    modalTitulo.innerText = "Repositório não possui Demo"
+                    modalErro.classList.remove("modal-on")
+                    modalErro.classList.add("modal-on")
+                    return setTimeout(()=>{
+                        modalErro.classList.remove("modal-on")
+                    }, 3000)
+                }
+                window.open(element.homepage, "_blank");
+            })
+
+            loading.classList.remove("flex-on")
+        });
+
+    }catch {
+        console.log('erro no repositorio');
+        }
+}
+userRepositorios(userLogado)
+trocarPerfil.addEventListener("click",()=>{
+    localStorage.removeItem("userLogado")
+    window.location.replace("../../index.html")
+})
